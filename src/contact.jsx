e40bb5cf7126424b73ef7d08f4176477487ef4a4
Alok Rawat <alokrawat2121@gmail.com>
2024-08-16 15:04:22 +0530

    completed

---
+++ src/contact.jsx
@@ -0,0 +1,170 @@
+import localforage from "localforage";
+import { matchSorter } from "match-sorter";
+import sortBy from "sort-by";
+import { Form, useFetcher, useLoaderData } from "react-router-dom";
+
+export async function loader({ params }) {
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  return { contact };
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
+
+export default function Contact() {
+  const { contact } = useLoaderData();
+
+  return (
+    <div id="contact">
+      <div>
+        <img
+          key={contact?.avatar}
+          src={
+            contact?.avatar ||
+            `https://robohash.org/${contact?.id}.png?size=200x200`
+          }
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact?.first || contact?.last ? (
+            <>
+              {contact?.first} {contact?.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}
+        </h1>
+        <h1>
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact?.twitter && (
+          <p>
+            <a target="_blank" href={`https://twitter.com/${contact?.twitter}`}>
+              {contact?.twitter}
+            </a>
+          </p>
+        )}
+
+        {contact?.notes && <p>{contact?.notes}</p>}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+          <Form
+            method="post"
+            action="destroy"
+            onSubmit={(event) => {
+              if (!confirm("Please confirm you want to delete this record.")) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Favorite({ contact }) {
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
+  return (
+    <fetcher.Form method="post">
+      <button
+        name="favorite"
+        value={favorite ? "false" : "true"}
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </fetcher.Form>
+  );
+}
+
+export async function getContacts(query) {
+  await fakeNetwork(`getContacts:${query}`);
+  let contacts = await localforage.getItem("contacts");
+  if (!contacts) contacts = [];
+  if (query) {
+    contacts = matchSorter(contacts, query, { keys: ["first", "last"] });
+  }
+  return contacts.sort(sortBy("last", "createdAt"));
+}
+
+export async function createContact() {
+  await fakeNetwork();
+  let id = Math.random().toString(36).substring(2, 9);
+  let contact = { id, createdAt: Date.now() };
+  let contacts = await getContacts();
+  contacts.unshift(contact);
+  await set(contacts);
+  return contact;
+}
+
+export async function getContact(id) {
+  await fakeNetwork(`contact:${id}`);
+  let contacts = await localforage.getItem("contacts");
+  let contact = contacts.find((contact) => contact.id === id);
+  return contact ?? null;
+}
+
+export async function updateContact(id, updates) {
+  await fakeNetwork();
+  let contacts = await localforage.getItem("contacts");
+  let contact = contacts.find((contact) => contact.id === id);
+  if (!contact) throw new Error("No contact found for", id);
+  Object.assign(contact, updates);
+  await set(contacts);
+  return contact;
+}
+
+export async function deleteContact(id) {
+  let contacts = await localforage.getItem("contacts");
+  let index = contacts.findIndex((contact) => contact.id === id);
+  if (index > -1) {
+    contacts.splice(index, 1);
+    await set(contacts);
+    return true;
+  }
+  return false;
+}
+
+function set(contacts) {
+  return localforage.setItem("contacts", contacts);
+}
+
+// fake a cache so we don't slow down stuff we've already seen
+let fakeCache = {};
+
+async function fakeNetwork(key) {
+  if (!key) {
+    fakeCache = {};
+  }
+
+  if (fakeCache[key]) {
+    return;
+  }
+
+  fakeCache[key] = true;
+  return new Promise((res) => {
+    setTimeout(res, Math.random() * 800);
+  });
+}
---
+++ src/main.jsx
@@ -0,0 +1,56 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path="contacts/:contactId/destroy" action={destroyAction} />
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
